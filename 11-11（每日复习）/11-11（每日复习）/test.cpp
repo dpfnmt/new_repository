cf807bbf1c977ca5711bdffc68280de26bebe79b
dpfnmt <1097722509@qq.com>
2019-11-11 20:59:26 +0800

    AVL树的复习代码

---
+++ 11-11（每日复习）/11-11（每日复习）/test.cpp
@@ -0,0 +1,158 @@
+#pragma once
+
+template<class K, class V>
+struct AVLTreeNode
+{
+	AVLTreeNode<K, V>* _left;
+	AVLTreeNode<K, V>* _right;
+	AVLTreeNode<K, V>* _parent;
+
+	std::pair<K, V> _kv;
+	int _bf; // balance factor
+};
+
+template<class K, class V>
+class AVLTree
+{
+	typedef AVLTreeNode<K, V> Node;
+public:
+	AVLTree()
+		:_root(nullptr)
+	{}
+
+	bool Insert(const std::pair<K, V>& kv)
+	{
+		if (_root == nullptr)
+		{
+			_root = new Node(kv);
+			return true;
+		}
+
+		Node* parent = nullptr;
+		Node* cur = _root;
+		while (cur)
+		{
+			if (cur->_kv.first > kv.first)
+			{
+				parent = cur;
+				cur = cur->_left;
+			}
+			else if (cur->_kv.first < kv.first)
+			{
+				parent = cur;
+				cur = cur->_right;
+			}
+			else
+			{
+				return false;
+			}
+		}
+
+		cur = new Node(kv);
+		if (parent->_kv.first < kv.first)
+		{
+			parent->_right = cur;
+			cur->_parent = parent;
+		}
+		else
+		{
+			parent->_left = cur;
+			cur->_parent = parent;
+		}
+
+		// 更新平衡因子
+		while (parent)
+		{
+			// 1.cur是parent的左边--bf cur是parent的右边++bf
+			if (cur == parent->_left)
+				parent->_bf--;
+			else
+				parent->_bf++;
+
+			// 1.parent == 0 高度不变，更新结束
+			// 2.parent == 1、-1.高度变了，继续更新
+			// 2.parent == 2、-2.不平衡，旋转变成平衡树，降低高度
+			if (parent->_bf == 0)
+			{
+				break;
+			}
+			else if (abs(parent->_bf) == 1)
+			{
+				cur = parent;
+				parent = parent->_parent;
+			}
+			else if (abs(parent->_bf) == 2)
+			{
+				// 旋转
+			}
+			else
+			{
+				assert(false);
+			}
+		}
+
+		return true;
+	}
+
+	void RotateL(Node* parent)
+	{
+		Node* subR = parent->_right;
+		Node* subRL = subR->_left;
+
+		parent->_right = subRL;
+		if (subRL)
+			subRL->_parent = parent;
+
+		subR->_left = parent;
+		Node* ppnode = parent->_parent;
+		parent->_parent = subR;
+
+		if (_root == parent)
+		{
+			_root = subR;
+			_root->_parent = nullptr;
+		}
+		else
+		{
+			if (ppnode->_left == parent)
+				ppnode->_left = subR;
+			else
+				ppnode->_right = subR;
+
+			subR->_parent = ppnode;
+		}
+
+		parent->_bf = subR->_bf = 0;
+	}
+
+	void RotateR(Node* parent)
+	{
+		Node* subL = parent->_left;
+		Node* subLR = subL->_right;
+
+		parent->_left = subLR;
+		if (subLR)
+			subLR->_parent = parent;
+
+		subL->_right = parent;
+		Node* ppnode = parent->_parent;
+		parent->_parent = subL;
+		if (parent == _root)
+		{
+			_root = subL;
+		}
+		else
+		{
+			if (ppnode->_left == parent)
+				ppnode->_left = subL;
+			else
+				ppnode->_right = subL;
+		}
+
+		subL->_parent = ppnode;
+		subL->_bf = parent->_bf = 0;
+	}
+
+private:
+	Node* _root;
+};
