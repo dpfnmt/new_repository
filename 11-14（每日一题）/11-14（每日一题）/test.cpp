f279b71aad1d1386bdf3749b4fc40ba6e8ae7797
dpfnmt <1097722509@qq.com>
2019-11-14 22:28:33 +0800

    每日复习

---
+++ 11-14（每日一题）/11-14（每日一题）/test.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+
+//class Car
+//{
+//public:
+//	virtual void Drive() = 0;
+//};
+//class Benz :public Car
+//{
+//public:
+//	virtual void Drive()
+//	{
+//		cout << "Benz-ÊæÊÊ" << endl;
+//	}
+//};
+//class BMW :public Car
+//{
+//public:
+//	virtual void Drive()
+//	{
+//		cout << "BMW-²Ù¿Ø" << endl;
+//	}
+//};
+//void Test()
+//{
+//	Car* pBenz = new Benz;
+//	pBenz->Drive();
+//	Car* pBMW = new BMW;
+//	pBMW->Drive();
+//}
+//
+//int main()
+//{
+//	Test();
+//	return 0;
+//}
+
+//class Car
+//{
+//public:
+//	virtual void Drive() final {}
+//};
+//class Benz :public Car
+//{
+//public:
+//	virtual void Drive() { cout << ¡°Benz - ÊæÊÊ¡± << endl; }
+//};
