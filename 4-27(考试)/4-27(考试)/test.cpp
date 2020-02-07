080febd97b98ec38f1a3faf7a23d8c443502f384
dpfnmt <1097722509@qq.com>
2020-02-07 18:16:14 +0800

    考试

---
+++ 4-27(考试)/4-27(考试)/test.cpp
@@ -0,0 +1,100 @@
+#include<iostream>
+#include<vector>
+#include<string>
+using namespace std;
+//int len = str.length();
+//char[] res = { '%', '2', '0' };
+//for (int i = 0; i < len; i++)
+//{
+//	if (str.charAt(i) == ' ')
+//	{
+//		str.deleteCharAt(i);
+//		str.insert(i, res);//第一个位置是offset，也不知道是不是这个不对？
+//	}
+//}
+//class StringFormat {
+//public:
+//	string formatString(string A, int n, vector<char> arg, int m) {
+//		vector<int> v;
+//
+//		n = A.length() - 1;
+//		 age = {}
+//		n = v.size() - 1;
+//		m = arg.size() - 1;
+//		while (v[n])
+//		{
+//			for (int i = 0; i <= n; i++)
+//			{
+//				if (v[i] != '%s')
+//				{
+//					++v[i];
+//				}
+//				else
+//				{
+//					v[i] = arg[m];
+//					++arg[m];
+//				}
+//			}
+//			//return arg[m];
+//		}
+//	}
+//};
+
+
+//class Gift {
+//public:    
+//	int getValue(vector<int> gifts, int n) 
+//	{                
+//		if(gifts.size() < n) 
+//			return 0;        
+//		if(gifts.size() == 0) 
+//			return 0;        
+//		int count = 0, temp;
+//		for(int i=0;i<n;i++)        
+//		{            
+//			if(count==0)            
+//			{                
+//				temp = gifts[i];                
+//				count = 1;
+//			}            
+//			else{                
+//				if(temp == gifts[i])                   
+//					count++;                
+//				else                   
+//					count--;            
+//			}        
+//		}        
+//		int size=0;        
+//		for(int i=0;i<n;i++){                      
+//			if(temp == gifts[i])               
+//				size++;        
+//		}        
+//		return (size>n / 2) ? temp : 0;
+//	}
+//};
+//int len = str.length();
+//char[] res = { '%', '2', '0' };
+//for (int i = 0; i < len; i++)
+//{
+//	if (str.charAt(i) == ' ')
+//	{
+//		str.deleteCharAt(i);
+//		str.insert(i, res);//第一个位置是offset，也不知道是不是这个不对？
+//	}
+//}
+
+//return str.toString();
+class StringFormat {
+public:
+	string formatString(string A, int n, vector<char> arg, int m) {
+		// write code here
+		0
+	}
+};
+
+int main()
+{
+	StringFormat s;
+	//s.formatString(");
+	return 0;
+}
