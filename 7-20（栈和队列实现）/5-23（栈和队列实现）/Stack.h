0e9d2d635558507e371c6394423f118947221837
dpfnmt <1097722509@qq.com>
2020-07-20 21:39:09 +0800

    算法的实现

---
+++ 7-20（栈和队列实现）/5-23（栈和队列实现）/Stack.h
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<assert.h>
+#include<stdlib.h>
+
+typedef int STDateType;
+typedef struct Stack
+{
+	STDateType * _a;
+	int _top; //Õ»¶¥
+	int _capacity;  //ÈÝÁ¿
+}Stack;
+
+
+
