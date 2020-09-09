129bedb1350704b67f261369065f7401ec38075f
dpfnmt <1097722509@qq.com>
2020-09-09 21:49:45 +0800

    栈的描写

--- 9-8（算法练习）/9-8（算法练习）/test.cpp
+++ 9-8（算法练习）/9-8（算法练习）/test.cpp
@@ -9,10 +9,15 @@ int  SelectNum(int n)
 	int sum = 0;
 	while (n > 1)
 	{
+		//喝过收集之后的瓶盖
 		int n1 = n / 3;
+		//剩余的瓶盖
 		int n2 = n % 3;
+		//总共能喝瓶数
 		sum = sum + n1;
+		//还能兑换的瓶盖
 		n = n1 + n2;
+		//如果还剩下两个，那么在总数上在加1
 		if (n == 2)
 		{
 			++sum;
